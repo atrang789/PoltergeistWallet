5d79a9f6cc320e3e24d6998a5d52495042de67fe
atrang789 <andrew.trang.thanh@gmail.com>
2022-01-21 16:05:53 -0600

    wrapped buttons into list widget

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -6,13 +6,14 @@ import 'package:poltergeistwallet/constants.dart';
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
 
-
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  final String _createwallet = 'Create Wallet';
+  final String _createWalletText = 'Create New Wallet';
+  final String _importWalletText = 'Import Existing Wallet';
+  final String _manageButtonText = 'Manage';
 
   @override
   Widget build(BuildContext context) {
@@ -21,25 +22,51 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           gradient: LinearGradient(
               begin: Alignment.bottomRight,
               end: Alignment.topLeft,
-              colors: [kPrimaryColor, kDarkSecondaryColor]
-          )
-      ),
+              colors: [kPrimaryColor, kDarkSecondaryColor])),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(title: Text('POLTERGEIST WALLET',)),
+        appBar: AppBar(
+            title: Text(
+          'POLTERGEIST WALLET',
+        )),
         body: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              SquareButton(
-                buttonTitle: _createwallet,
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              )
-            ]
-        ),
+              Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        Expanded(
+                          child: SquareButton(
+                            buttonTitle: _createWalletText,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: SquareButton(
+                            buttonTitle: _importWalletText,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: SquareButton(
+                            buttonTitle: _manageButtonText,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    )
+              )),
+            ]),
       ),
     );
-
   }
 }
