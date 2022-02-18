bae6da83f719f43b3ed09ee2790fe0a8f6d56a5a
atrang789 <andrew.trang.thanh@gmail.com>
2022-02-18 15:23:58 -0600

    updated welcome view to contain list and navigate to wallet details

---
+++ lib/screens/wallet_detail_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WalletDetail extends StatefulWidget {
+  @override
+  _WalletDetailState createState() => _WalletDetailState();
+}
+
+class _WalletDetailState extends State<WalletDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
